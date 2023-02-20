2788a3930b60f437820150ca6fcb1a83098e3c4e
M-Zeeshan6886 <mzxeeshuu@gmail.com>
2023-02-21 03:30:13 +0500

    SubHeader Done

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,8 @@
 import Picture from "./react.svg";
 import Logo from "./navAssets/GPTlogo.png";
+import Subheaderimg from './subnavAssets/subImg.png'
+import Pf1 from './subnavAssets/pf1.png';
+import Pf2 from './subnavAssets/pf2.png';
+import Pf3 from './subnavAssets/pf3.png';
 
-export { Picture, Logo };
+export { Picture, Logo, Subheaderimg, Pf1,Pf2,Pf3 };
--- src/components/modules/home/Home.jsx
+++ src/components/modules/home/Home.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
-import './Home.scss';
+import React from "react";
+import "./Home.scss";
+import { Subheaderimg } from "../../../assets";
+import { Pf1 } from "../../../assets";
+import { Pf2 } from "../../../assets";
+import { Pf3 } from "../../../assets";
 
 const Home = () => {
   return (
@@ -14,12 +18,35 @@ const Home = () => {
               alteration boisterous the attachment. Party we years to order
               allow asked of.
             </p>
+            <div className="subheader-container-content-left-infield">
+              <input type="search" placeholder="Your Email Address" />
+              <button>Get Started</button>
+            </div>
+            <div className="subheader-container-content-left-pffield">
+              <div className="subheader-container-content-left-pffield-left">
+                <div className="subheader-container-content-left-pffield-left-imgfield">
+                  <img src={Pf1} alt="photo1" />
+                  <img src={Pf3} alt="photo1" />
+                  <img src={Pf2} alt="photo1" />
+                  <img src={Pf3} alt="photo1" />
+                  <img src={Pf2} alt="photo1" />
+                  <img src={Pf1} alt="photo1" />
+                </div>
+                <span>1.6k+</span>
+              </div>
+
+              <div className="subheader-container-content-left-pffield-right">
+                <p>1,600 people requested access a visit in last 24 hours</p>
+              </div>
+            </div>
           </div>
-          <div className="subheader-container-content-right"></div>
+          <div className="subheader-container-content-right">
+            <img src={Subheaderimg} alt="Picture" />
+          </div>
         </div>
       </div>
     </>
   );
-}
+};
 
-export default Home
+export default Home;
