c32c3bcf812375dfaaf5354c1ad8cc6b92d62102
M-Zeeshan6886 <mzxeeshuu@gmail.com>
2023-02-21 01:09:23 +0500

    Navbar Done

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from "react";
+import "./Navbar.scss";
+import { Logo } from "../../../assets";
 
 const Navbar = () => {
   return (
     <>
-    <div className='nav-container'>
-        <div className='nav-container-content'>
-            
+      <div className="nav-container">
+        <div className="nav-container-content">
+          <div className="nav-container-content-left">
+            <img src={Logo} alt="GPTLOGO" />
+          </div>
+          <div className="nav-container-content-middle">
+            <button>Home</button>
+            <button>What is GTP?</button>
+            <button>Open AI</button>
+            <button>Case Studies</button>
+            <button>Library</button>
+          </div>
+          <div className="nav-container-content-right">
+            <button>Sign in</button>
+            <button>Sign up</button>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
